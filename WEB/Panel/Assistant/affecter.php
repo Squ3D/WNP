1a2e9eb608393c60a402d350d1aa093ca104e2a1
Naïm <37388224+Squ3D@users.noreply.github.com>
2020-01-05 21:30:00 +0100

    Génération fiche PDF en cours.
    --> Génération des Pdfs par les assistants.
    
    (Séléction directement des Techniciciens Disponibles, et des clients ayant besoin d'une révision à partir de la bdd.)
    
    -> En cours génération en PDF de la fiche d'intervention.

--- WEB/Panel/Assistant/affecter.php
+++ WEB/Panel/Assistant/affecter.php
@@ -99,10 +99,11 @@ $dato = $sql1->fetchColumn();
                 ?>
             </h1>
             
-            <fieldset>
-          <legend>Seléctionner un Technicien</legend>
-          <p>
-             <label>Matricule</label>
+           
+       
+       <form name="watka" method="post">
+
+             <label>Technicien Dispo</label>
              <select id = "myList">
              <?php
 $dati = $conn->query('SELECT * FROM Technicien');
@@ -113,15 +114,57 @@ while ($donnees = $dati->fetch())
            <option value=" <?php echo $donnees['Nom']; ?>"> <?php echo $donnees['Nom']; ?></option>
 <?php
 }
- 
-$reponse->closeCursor();
 ?>
+
+
              </select>
           </p>
           
           
        </fieldset>
-       <input method="post" type="submit" class="fadeIn fourth">     
+    
+    <label>A visiter</label>
+               <select id = "myList">
+             <?php
+$datop = $conn->query('SELECT * FROM Client');
+ 
+while ($donnees = $datop->fetch())
+{
+?>
+           <option value=" <?php echo $donnees['Nom']; ?>"> <?php echo $donnees['Raison']; ?></option>
+<?php
+}
+?>
+
+<?php
+
+define("FPDF_FONTPATH"," Chemin_du_répertoire/font/"); 
+//lien vers le dossier " font " 
+require("Chemin_du_répertoire/fpdf.php"); 
+//lien vers le fichier contenant la classe FPDF
+
+$pdf = new FPDF("P","pt","A4"); 
+//création d'une instance de classe, P pour portrait
+
+$pdf ->Open(); //indique que l'on crée un fichier PDF
+$pdf ->AddPage(); //permet d'ajouter une page
+$pdf ->SetFont('Helvetica','B',11); //choix de la police
+$pdf ->SetXY(330, 25); // indique des coordonnées pour 
+$pdf ->Cell(190,50,"texte dans le cadre",0,0, "L"); 
+$pdf ->Text(498,20, "texte"); //insertion d'une ligne 
+$pdf ->Output(); //génère le PDF et l'affiche	
+
+?>
+
+
+             </select>
+        
+
+
+     
+        </p>
+    </form>
+       <input name="hachik" method="post" type="submit" class="fadeIn fourth">     
             </center>
 
         <p>

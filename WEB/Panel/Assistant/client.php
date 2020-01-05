1ad08de3802c3f4b99ca175e38468ca3a93d80e7
wendyblt <wendy.billet@gmail.com>
2020-01-05 22:46:49 +0100

    Correction création intervention

--- WEB/Panel/Assistant/client.php
+++ WEB/Panel/Assistant/client.php
@@ -3,20 +3,26 @@ session_start();
 require_once "../../config.php";
 
 //Utiliser l'extract $POST
-$numFiche = $_POST['1'];
-$adresse = $_POST['2'];
-$date_visite = $_POST['3'];
-$heure_visite = $_POST['4'];
-$Matricule = $_POST['5'];
-$code_Client = $_POST['6'];
 
-if (isset($_POST['upload'])) {
 
-    $query = $conn->prepare("INSERT INTO Intervention (Numero_Fiche, adresse, date_visite, heure_visite,Num_Matricule, Code_Client)
-      VALUES ($numFiche, $adresse, $date_visite,$heure_visite,$Matricule,$code_Client)");
-    $query->execute();
-} else {
-    echo 'fail during execution';
+if (isset($_POST['create'])) {
+    // vérifier que tous les champs soient saisis
+    $numFiche = $_POST['1'] ?? die("Num Fiche manquant");
+    $adresse = $_POST['2'] ?? die("Adresse manquante");
+    $date_visite = $_POST['3'] ?? die(" Date de visite manquante");
+    $heure_visite = $_POST['4'] ?? die("Heure de visite manquante");
+    $Matricule = $_POST['5'] ?? die("Numéro de matricule manquant");
+    $code_Client = $_POST['6'] ?? die("Numéro client  manquant");
+
+
+    $query = $conn->prepare("INSERT INTO Intervention(Numero_Fiche, adresse, date_visite, heure_visite,Num_Matricule, Code_Client)
+      VALUES ('$numFiche', '$adresse', '$date_visite','$heure_visite',$Matricule,$code_Client)");
+
+    if ($query->execute()) {
+        echo "<script>alert(\"Informations enregistrées\")</script>";
+    } else {
+        echo "Erreur lors de la création d'une intervention";
+    }
 }
 ?>
 
@@ -69,22 +75,33 @@ if (isset($_POST['upload'])) {
 
 <div id="box">
     <form name="watka" method="post">
+        <label>Numéro</label>
+        <input type="text" id="login" class="fadeIn second" name="1"
+               placeholder="Renseigner le numéro de fiche" size="200"
+               required>
+        <br>
+        <label>Adresse</label>
+        <input type="text" id="password" class="fadeIn third" name="2" placeholder="adresse" required>
+        <br>
+        <label>Heures</label>
+        <input type="text" id="password" class="fadeIn third" name="4" placeholder="Renseigner l'heure de visite"
+               required>
+        <br>
+        <label>Matric</label>
+        <input type="number" id="login" class="fadeIn second" name="5" placeholder="Renseigner Numéro de Matricule"
+               required>
+        <br>
+        <label>Cclient</label>
+        <input type="number" id="password" class="fadeIn third" name="6" placeholder="Renseigner le code Client"
+               required>
+        <br>
+        <label>Date</label>
+        <input type="date" id="login" class="fadeIn second" name="3" placeholder="Renseigner la date de visite"
+               required>
+        <br>
 
+        <input name="create" method="post" type="submit" class="fadeIn fourth">
 
-        <p>Numéro <input type="text" id="login" class="fadeIn second" name="1"
-                         placeholder="Renseigner le numéro de fiche" size="200"></p>
-        <p>Adresse <input type="text" id="password" class="fadeIn third" name="2" placeholder="adresse"></p>
-        <p>Heures <input type="text" id="password" class="fadeIn third" name="4"
-                         placeholder="Renseigner l'heure de visite"></p>
-        <p>Matric <input type="text" id="login" class="fadeIn second" name="5"
-                         placeholder="Renseigner Numéro de Matricule"></p>
-        <p>Cclient <input type="text" id="password" class="fadeIn third" name="6"
-                          placeholder="Renseigner le code Client"></p>
-        <p><input type="date" id="login" class="fadeIn second" name="3" placeholder="Renseigner la date de visite"></p>
-
-
-        <input name="wata" method="post" type="submit" class="fadeIn fourth"></a>
-        </p>
     </form>
 
 
